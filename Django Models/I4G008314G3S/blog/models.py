81d3d55361499b20e3fa474a91ca383dfbb30ba3
Adesina <user@users-MacBook-Pro.local>
2022-06-22 16:12:56 +0100

    final

---
+++ Django Models/I4G008314G3S/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField(null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(auto_now=True)
+
